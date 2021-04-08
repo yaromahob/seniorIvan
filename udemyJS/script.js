420ba13c3e931c2a18dc960463250c9976dede47
Alexandr <yaromahob@gmail.com>
2021-04-08 23:39:01 +0300

    codeshatal

---
+++ udemyJS/script.js
@@ -0,0 +1,30 @@
+console.log("5 % 2", 5 % 2);
+console.log("8 % 3", 8 % 3);
+let i = 1;
+//i = i + 1;
+i++;
+console.log("i", i);
+
+// Cаша
+// код
+// ваш
+// шатал
+
+/* 
+Саша
+код
+Ваш
+расшатал
+*/
+
+/*
+
+Number - 123
+
+String - 'string'
+
+Boolean - true(1) folse(0)
+
+Null - 
+
+*/
